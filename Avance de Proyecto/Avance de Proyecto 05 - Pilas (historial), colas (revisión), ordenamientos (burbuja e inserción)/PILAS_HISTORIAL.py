e2c77e2cf166e22ae07e38c3e05f422dca01a2a0
eduardoyoc519-oss <eduardoyoc519@gmail.com>
2025-10-03 21:59:44 -0600

    Add files via upload

---
+++ Avance de Proyecto/Avance de Proyecto 05 - Pilas (historial), colas (revisión), ordenamientos (burbuja e inserción)/PILAS_HISTORIAL.py
@@ -0,0 +1,24 @@
+# implementacion de una pila en para el proyecto 
+#este es para el historial de mi proyectoo 
+class pila:
+    def __init__(self):
+        self.items = []
+
+    def esta_vacia(self):
+        return len(self.items) == 0
+
+    def apilar(self, item):
+        self.items.append(item)
+        print(f"historial actualizado: {self.items}")
+
+    def desapilar(self):
+        if not self.esta_vacia():
+            return self.items.pop()
+        return None
+
+    def ver_tope(self):
+        if not self.esta_vacia():
+            return self.items[-1]
+        return None
+    
+    
